e2c4ebae940497445650b9ba6c5e67d2258a9d32
elixerofdoom <elixerofdoom@web.de>
2022-03-12 01:15:58 +0100

    keep going lad! You can do it!

--- main.py
+++ main.py
@@ -2,7 +2,20 @@
 import json
 import imdb_api as imdb
 
-def menu():
+
+
+def int_input(validate=int) -> int:
+    """Takes an int as ipt and validates it with the int() function and except ValueError.""" 
+    while True:
+        ipt = input()
+        try:
+            ipt = int(ipt)
+        except ValueError:
+            print('Invalid input! Try again.')
+            continue
+        return ipt
+
+def menu() -> str:
     """basic menu"""
     print(
         """
@@ -14,7 +27,7 @@ def menu():
     """)
     return  input()    
 
-def prt_list(chosen_list):
+def prt_list(chosen_list) -> None:
     """print either watchlist or finished-film-list. chosen_list = 'watchlist'/'watched_list'
     w/ index and year """
     with open("database.txt", "r") as jsonFile:
@@ -22,33 +35,53 @@ def prt_list(chosen_list):
         for index, item in enumerate(watchlist[chosen_list]):
             print(index, item['title'], item['description'])
 
+def search_imdb(search_type, title) -> dict:
+    """looks up imdb with imdb_api module"""
+    response_obj = imdb.get_respObj(search_type, title)
+    return imdb.get_jsonObj(response_obj)
+
 def add_item():
+    """adds item from imdb to the list"""
+    # input stuff
     while True:
         user_ipt = input("Film(1) or TV(2)?: ")
         if user_ipt == "1":
             search_type = "SearchMovie"
-            title = input("What is the title of the Film?: ")
+            title = input("What is the title of the film?: ")
             break
         elif user_ipt == "2":
             search_type = "SearchSeries"
-            title = input("What is the title of the Show?: ")
+            title = input("What is the title of the series?: ")
             break
         else:
             continue
+    #filtering results
+    search_result = search_imdb(search_type, title) 
+    for index, item in enumerate(search_result['results']):
+        print(f"({index}) - {item['title']}, {item['description']}")
+    #input stuff w/ validating
+    while True:
+        ipt = int_input()
+        if -1 <= ipt <= len([search_result['results']])-1: # check if ipt is in range of results:
+            break
+        else:
+            print("Invalid input. Try again.")
+            continue
+    
+    chosen_item = search_result['results'][ipt]
+    with open("database.txt", "r+") as jsonDB:
+        dictDB = json.load(jsonDB)
+        dictDB['watchlist'].append(chosen_item)
+        json.dump(dictDB, jsonDB, sort_keys=True, indent=4) # dump with pretty format
 
-    response_obj = imdb.get_respObj(search_type, title)
-    print(imdb.get_jsonObj(response_obj))
-    
-    
 def remove_item():
     """prints watchlist with an index. Put in index to delete item from list and return it"""
     prt_list("watchlist")
     with open("database.txt", "r+") as jsonFile:
         watchlist = json.load(jsonFile)
         type(watchlist)
-
+        #TODO: Implement removing item
         
-
 def main():
     running = True
     while running:
