49f9af7db1c2890672f6a517050d2d31df0be7eb
Elixerofdoom <elixerofdoom@web.de>
2022-05-25 00:05:46 +0200

    added json database file.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # A watchlist organizer in PySimpleGUI
 import json
+from typing import Tuple
 import PySimpleGUI as sg
 from classes import Movie
+import imdb_api as imdb
 
 def create_window() -> sg.Window:
     """ Layouts are stored locally else an Error message occurs
@@ -22,20 +24,8 @@ def create_window() -> sg.Window:
                 expand_x=True,
                 key='TEXT',)
         ],
-        [  # Row 2
-            sg.Table(
-                values= table_content_watched,
-                headings= headings_watched,
-                text_color= 'Black',
-                def_col_width= 30,
-                row_height= 20,
-                num_rows= 20,
-                background_color= 'grey',
-                auto_size_columns= False,
-                justification= 'right',
-                key= '-WATCHED_LIST_TABLE-'
-                ),
-            sg.Table(
+        [  # Row 2 
+            sg.Table( # not watched
                 values= table_content_not_watched,
                 headings= headings_not_watched,
                 text_color= 'Black',
@@ -43,27 +33,29 @@ def create_window() -> sg.Window:
                 row_height= 20,
                 num_rows= 20,
                 background_color= 'grey',
+                display_row_numbers= True,
                 auto_size_columns= False,
+                right_click_menu= right_click_menu_1,
                 justification= 'right',
                 key= '-WATCHLIST_TABLE-'
                 ),
+            sg.Table( # watched
+                values= table_content_watched,
+                headings= headings_watched,
+                text_color= 'Black',
+                def_col_width= 30,
+                row_height= 20,
+                num_rows= 20,
+                background_color= 'grey',
+                display_row_numbers= True,
+                auto_size_columns= False,
+                right_click_menu= right_click_menu_2,
+                justification= 'right',
+                key= '-WATCHED_LIST_TABLE-'
+                ),
         ], 
         [   # Row 3
-            sg.Button(
-                'move', key='-MOVE-'
-                ), 
-            sg.Button(
-                'edit', key='-EDIT-'
-                ),
-            sg.Button(
-                'remove', key='-REMOVE-'
-            ),
             sg.Push(),
-            sg.Button(
-                'edit', key='-EDIT2-'
-            )
-        ],
-        [   # Row 4
             sg.Button(
                 button_text= 'exit', 
                 key= '-EXIT-')
@@ -101,18 +93,57 @@ def create_movie_objects(database) -> None:
             m.set_watched_date(item['watched date'])
         watched_list.append(m)
 
-def fill_tables() -> None:
-    """ call this only after create_movies_objects()"""
-    for item in watchlist:
-        table_content_watched.append([item.title])
-    for item in watched_list:
-        table_content_not_watched.append([item.title])
+def show_details():
+    pass
+
+def update_tables() -> None:
+    global table_content_not_watched, table_content_watched
+    table_content_not_watched = []
+    table_content_watched = []
+    for movie in watchlist:
+        table_content_not_watched.append([movie.title])
+    for movie in watched_list:
+        table_content_watched.append([movie.title])
+
+def move_movie(remove_from, put_in, indx) -> None:
+    m = remove_from.pop(indx)
+    put_in.append(m)
+    
+def get_input() -> Tuple[str, str]:
+    """returned Tuple first value shows if show (True) or movie (False) and second val is name"""
+    popup = sg.Window('Continue?', [
+        [sg.Radio('Show', "RAD1"), sg.Radio('Movie', "RAD1")], 
+        [sg.Input(key='-INPUT-')],
+        [sg.Button('Cancel'), sg.Push(), sg.Button('Submit')]])
+    while True:
+        event, values = popup.read()
+        if event == sg.WINDOW_CLOSED or event == 'Cancel':
+            popup.close()
+            return
+        if event == 'Submit':
+            if values['-INPUT-'] == '':
+                continue
+            if values[0] == False and values[1] == False:
+                print('both radio')
+                continue
+            break
+        print(event, values)
+    
+    popup.close()
+    if values[0] == True:
+        return 'SearchShow', values['-INPUT-']
+    else:
+        return 'SearchMovie', values['-INPUT-']
+
+def display_results():
+    # TODO
+    pass
 
 def main() -> None:
     """Main Function where all the logic is"""
     database = load_json()
     create_movie_objects(database)
-    fill_tables()
+    update_tables()
     window = create_window()
 
     while True:
@@ -122,12 +153,40 @@ def main() -> None:
         if event == sg.WIN_CLOSED or event == '-EXIT-':
             break
         
-        if event == '-EDIT-':
-            pass
+        if event == 'Add':
+            ipt = get_input()
+            if ipt == None:
+                continue
+            display_results(ipt)
+            
 
-        if event == '-MOVE-':
+
+
+
+
+
+
+        if event == 'Move':
+            if values['-WATCHLIST_TABLE-'] == []:
+                continue
+            move_movie(watchlist, watched_list, values['-WATCHLIST_TABLE-'][0])
+            update_tables()
+            window['-WATCHLIST_TABLE-'].update(table_content_not_watched)
+            window['-WATCHED_LIST_TABLE-'].update(table_content_watched)
+
+        if event == 'Move Back':
+            if values['-WATCHED_LIST_TABLE-'] == []:
+                continue
+            move_movie(watched_list, watchlist, values['-WATCHED_LIST_TABLE-'][0])
+            window['-WATCHLIST_TABLE-'].update(table_content_not_watched)
+            window['-WATCHED_LIST_TABLE-'].update(table_content_watched)
+
+        if event == 'Remove':
             pass
         
+        if event == 'Details':
+            show_details()
+
         print(event, values)
 
     window.close()
@@ -136,7 +195,14 @@ def main() -> None:
 # variables
 font= ('Ariel, 12')
 
-menu_def = [['Edit', ['Add', 'Remove']],
+menu_def = [['File', ['Add']],
+            ['Help', ['right click on item for drop down menu']]
+]  
+right_click_menu_1 = ['rc_menu',
+    ['Move', 'Details', '---', 'Remove']
+]
+right_click_menu_2 = ['rc_menu',
+    ['Move Back', 'Details']
 ]
 
 table_content_watched = [] # 2d array
